741a848877fd02c1157b6d633f91036ca9578522
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-12 11:10:02 -0300

    refactor: remove responsabilidade de remover livro do bibliotecario

--- src/system_of_library/funcionario/Bibliotecario.java
+++ src/system_of_library/funcionario/Bibliotecario.java
@@ -1,13 +1,12 @@
 package system_of_library.funcionario;
+import system_of_library.model.Book;
 import system_of_library.service.Library;
 import system_of_library.usuarios.Usuario;
 public class Bibliotecario extends Usuario {
 
     private Library library;
-
     public Bibliotecario (String name, String email,String endereco ,Library library) {
         super(name,email,endereco);
-
         this.library = library;
     }
 
@@ -20,9 +19,6 @@ public class Bibliotecario extends Usuario {
         library.createBook();
     }
 
-    public void removerLivro(int id){
-        library.removeBook(id);
-    }
 
 }
 
