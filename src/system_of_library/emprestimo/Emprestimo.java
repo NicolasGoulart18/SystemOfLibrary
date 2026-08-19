cbce934ea96716454f6acdfa6a9879566be6bf98
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-19 14:52:42 -0300

    Classe emprestimo melhorada, novas funções

--- src/system_of_library/emprestimo/Emprestimo.java
+++ src/system_of_library/emprestimo/Emprestimo.java
@@ -1,31 +1,25 @@
 package system_of_library.emprestimo;
+
 import system_of_library.model.Book;
 import system_of_library.usuarios.Usuario;
 import java.time.LocalDate;
-public class Emprestimo {
 
+public class Emprestimo {
     private Book livro;
     private Usuario usuario;
-    private LocalDate emprestimo;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucaoPrevista;
 
     public Emprestimo(Book livro, Usuario usuario) {
         this.livro = livro;
         this.usuario = usuario;
-        this.emprestimo=LocalDate.now();
+        this.dataEmprestimo = LocalDate.now();
+        // Polimorfismo sendo usado no cálculo da data limite:
+        this.dataDevolucaoPrevista = dataEmprestimo.plusDays(usuario.getPrazoEmprestimoDias());
     }
 
-
-    public Book getLivro() {
-        return livro;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-
-    public LocalDate getEmprestimo() {
-        return emprestimo;
-    }
-
+    public Book getLivro() { return livro; }
+    public Usuario getUsuario() { return usuario; }
+    public LocalDate getDataEmprestimo() { return dataEmprestimo; }
+    public LocalDate getDataDevolucaoPrevista() { return dataDevolucaoPrevista; }
 }
--- src/system_of_library/usuarios/Usuario.java
+++ src/system_of_library/usuarios/Usuario.java
@@ -47,5 +47,3 @@ public class Usuario {
         this.endereco = endereco;
     }
 }
-
-
