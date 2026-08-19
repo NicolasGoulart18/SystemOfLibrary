617c80199ac1989992440b12e5d6ead033dc4e36
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-19 14:48:49 -0300

    enum

--- src/system_of_library/emprestimo/ResultadoEmpr.java
+++ src/system_of_library/emprestimo/ResultadoEmpr.java
@@ -5,25 +5,4 @@ public enum ResultadoEmpr {
         SUCESSO,
         LIVRO_NAO_EXISTE,
         LIVRO_INDISPONIVEL
-
-   /*
-        O que é enum:
-            Serve para representar uma variavle que so pode assumir um conjunto especificos de valores que vc definiu
-
-            Por exemplo em Status de pedidos:
-            public enum StatusPedido{
-            PENDENTE,
-            PAGO,
-            ENVIADO,
-            ENTREGUE,
-            CANCELADO
-
-            Ai Status pedido virou um tipo ai posso fazer
-            StatusPedido status = StatusPedido.PAGO
-            }
-
-            Pq usar no meu projeto ?
-                Como eu preciso retorar 2 valores false, o basico boolean não permite, pois so return true/false
-                ent o enum entra como uma luva nesse momento
-    */
 }
