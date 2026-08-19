195f476adf77475faeac10305cd44a54e74c64ad
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-19 12:27:45 -0300

    Choice case 3->metodo

--- src/system_of_library/interaction/Choice.java
+++ src/system_of_library/interaction/Choice.java
@@ -1,5 +1,5 @@
 package system_of_library.interaction;
-
+import system_of_library.emprestimo.ResultadoEmpr;
 import system_of_library.service.Library;
 import system_of_library.usuarios.Aluno;
 import system_of_library.usuarios.Bibliotecario;
@@ -9,15 +9,11 @@ import system_of_library.usuarios.Usuario;
 import java.util.Scanner;
 
 public class Choice {
-
+    int id=0;
     static Scanner scanner = new Scanner(System.in);
-
+    static Library library = new Library();
     public static void main(String[] args) {
-
         Choice choice = new Choice();
-
-        Library library = new Library();
-
         Bibliotecario bibliotecario = new Bibliotecario("Jõao", "joao1232@email.com", "rua dos cuiudo 80", library
         );
 
@@ -93,6 +89,65 @@ public class Choice {
     }
 
     private void emprestarLivro() {
+        System.out.println("[1] Aluno | [2] Professor ");
+        int num = Integer.parseInt(scanner.nextLine());
+        Usuario usuario = null;
 
+        if (num == 1) {
+            System.out.println("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.println("Email: ");
+            String email = scanner.nextLine();
+
+            System.out.println("Endereço: ");
+            String endereco = scanner.nextLine();
+
+            System.out.println("Ano escolar: ");
+            String anoEscolar = scanner.nextLine();
+
+            System.out.println("Turma: ");
+            String turma = scanner.nextLine();
+
+            System.out.println("ID do livro desejado: ");
+            id = Integer.parseInt(scanner.nextLine());
+
+            usuario = new Aluno(nome, email, endereco, anoEscolar, turma);
+
+        } else if (num == 2) {
+            System.out.println("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.println("Email: ");
+            String email = scanner.nextLine();
+
+            System.out.println("Endereço: ");
+            String endereco = scanner.nextLine();
+
+            System.out.println("Disciplina:");
+            String disciplina = scanner.nextLine();
+
+            System.out.println("ID do livro desejado: ");
+            id = Integer.parseInt(scanner.nextLine());
+
+            usuario = new Professor(nome, email, endereco, disciplina);
+
+        } else {
+            System.out.println("Opção Inválida!");
+        }
+
+        if (usuario != null && id != 0) {
+            ResultadoEmpr resultadoEmpr = library.lendBook(id, usuario);
+
+            if (resultadoEmpr == ResultadoEmpr.SUCESSO) {
+                System.out.println("Livro emprestado com sucesso!");
+
+            } else if (resultadoEmpr == ResultadoEmpr.LIVRO_INDISPONIVEL) {
+                System.out.println("Livro indisponivel");
+
+            } else {
+                System.out.println("Livro não existe!");
+            }
+        }
     }
 }
