bc69645f66564b276d0c5079f4ecf91bfc1266df
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-19 14:53:52 -0300

    Aluno extends Usuario modificada

--- src/system_of_library/usuarios/Aluno.java
+++ src/system_of_library/usuarios/Aluno.java
@@ -1,30 +1,20 @@
 package system_of_library.usuarios;
 
-public class Aluno extends Usuario{
-
+public class Aluno extends Usuario {
     private String anoEscolar;
     private String turma;
 
-    public Aluno(String nome,String email,String endereco, String anoEscolar, String turma){
-        super(nome,email,endereco);
-        this.anoEscolar=anoEscolar;
-        this.turma=turma;
-    }
-
-
-    public String getAnoEscolar() {
-        return anoEscolar;
-    }
-
-    public void setAnoEscolar(String anoEscolar) {
+    public Aluno(String nome, String email, String endereco, String anoEscolar, String turma) {
+        super(nome, email, endereco);
         this.anoEscolar = anoEscolar;
-    }
-
-    public String getTurma() {
-        return turma;
-    }
-
-    public void setTurma(String turma) {
         this.turma = turma;
     }
+
+    @Override
+    public int getPrazoEmprestimoDias() {
+        return 7; // Alunos têm 7 dias de prazo
+    }
+
+    public String getAnoEscolar() { return anoEscolar; }
+    public String getTurma() { return turma; }
 }
